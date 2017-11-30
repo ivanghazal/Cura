a4d624cc8e959978fe0f8af35311ff7787315f80
ivan ghazal <ivanghazal@hotmail.com>
2017-11-30 08:21:31 -0800

    Initial Commit

---
+++ Cura/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cura
+//
+//  Created by Ivan Ghazal on 11/30/17.
+//  Copyright © 2017 Ivan Ghazal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cura/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cura
+//
+//  Created by Ivan Ghazal on 11/30/17.
+//  Copyright © 2017 Ivan Ghazal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
