c45dc5fae63243c1f89e2da26c978a6e0ba0d252
ivan ghazal <ivanghazal@hotmail.com>
2017-12-13 19:01:09 -0800

    fix textView

--- ViewController/conditionPage.swift
+++ ViewController/conditionPage.swift
@@ -15,7 +15,40 @@ class conditionPage: UIViewController {
     @IBOutlet weak var saveButton: UIButton!
     
     
+    
+    @IBOutlet weak var emergency: UITextView!
+    @IBOutlet weak var txtDescription: UITextView!
+    
+    
+    
     @IBOutlet weak var backButton: UIBarButtonItem!
+    
+    
+    let emergencytxt = """
+
+                    If you feel any heart attack symptoms, don't wait to get help.
+                    Call 911 immediately.
+                    Rest until an emergency team arrives.
+                    Paramedics may give you an adult-strength aspirin to chew on to help prevent blood clotting.
+                    If you are having chest pain and your doctor has prescribed nitroglycerin:
+                    Take one dose of nitroglycerin and let it dissolve under your tongue.
+                    Wait 5 minutes.
+                    If chest pain doesn't improve or gets worse, call 911. The emergency operator will tell you what to do.
+                    """
+    
+    let descriptionTxt = """
+                    Heart Attack (Female) Symptoms
+                    Discomfort, tightness, pressure, fullness, or crushing pain in the chest; pain in the back, jaw, neck, shoulders, or arms (which may radiate from the chest); shortness of breath; profuse sweating; nausea or vomiting; exhaustion or weakness; stomach or abdominal pain; anxiety; new or worse headaches
+
+                    Overview
+                    A heart attack occurs when blood flow to the heart muscle is reduced or stopped by blockage in one or more arteries. If blood supply is cut off for more than a few minutes, you may become disabled or die. Getting emergency treatment can limit or prevent damage to the heart and save your life. Women are less likely than men to believe they're having a heart attack and less likely get help right away. Some women mistakenly think heart attacks cause only chest pain. But a woman having a heart attack may just feel extreme weakness or fatigue, fullness in the chest, or back pain between the shoulder blades. Some may feel only nausea or shortness of breath. Women can't control some risk factors for heart attacks, such as age or heredity, but can control many others.
+
+                    Visit the Heart Attack Directory
+
+
+
+                    """
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +59,11 @@ class conditionPage: UIViewController {
         shareButton.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
         saveButton.layer.cornerRadius = 5
         saveButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        
+        //load data
+        emergency.text = emergencytxt
+        txtDescription.text = descriptionTxt
+        
     
     }
 
